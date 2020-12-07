8f21fb54486aae103bf0032abadfef0f204e9134
Larissa Silva <larissadantas22k@hotmail.com>
2020-12-07 03:38:07 -0300

    CobWeb

--- Vcom/VcomCalc/Page/VcomCalcPage.cs
+++ Vcom/VcomCalc/Page/VcomCalcPage.cs
@@ -13,6 +13,35 @@ namespace Vcom.Pages
         public By btnAtualizar = By.CssSelector("#btn-atualizar");
         public By btnFinalizarNegociacao = By.CssSelector("#btn-finalizar");
         public By btnVisualizarBoleto = By.CssSelector("div:nth-child(1) > p > a");
+        public By btnBoletoEmail = By.CssSelector("div:nth-child(2) > p > a");
+        public By selectEmail = By.CssSelector("div[class='col-md-6 col-sm-12'] select");
+        public By opcaoEmail = By.CssSelector("div[class='col-md-6 col-sm-12'] select > option:nth-child(3)");
+        public By btnEnviarEmail = By.CssSelector("#btn-envie-agora-email");
+        public By btnEnviarSMS = By.CssSelector("div:nth-child(3) > p > a");
+        public By selectNumero = By.XPath("//*[@class='envio-boleto-sms-form']/div/div[2]/div/div[1]/div[1]/select");
+        public By selectOpcaoNumero = By.XPath("//*[@class='envio-boleto-sms-form']/div/div[2]/div/div[1]/div[1]//option[3]");
+        public By selectEnvio = By.XPath("//*[@class='envio-boleto-sms-form']/div/div[2]/div/div[1]/div[3]/select");
+        public By selectOpcaoEnvio = By.XPath("//*[@class='envio-boleto-sms-form']/div/div[2]/div/div[1]/div[3]//option[2]");
+        public By selectSMS = By.XPath("//*[@class='envio-boleto-sms-form']/div/div[2]/div/div[2]/div/select");
+        public By selectOpcaoSMS = By.XPath("//*[@class='envio-boleto-sms-form']/div/div[2]/div/div[2]/div//option[2]");
+        public By btnEnviarSMSAgora = By.CssSelector("#btn-envie-agora-sms");
+        public By btnNegociacoes = By.CssSelector("#btn-ultimas-negociacoes");
+        public By btnMostrarOpcoes = By.XPath("//*[@id='tr-negociacao-dados-138669']/td[1]/a/i");
+        public By btnOpcoes = By.XPath("//*[@id='tr-negociacao-138669']/td[2]/div/div[1]/div/button");
+        public By btnReimprimir = By.XPath("//*[@id='tr-negociacao-138669']/td[2]/div/div[1]/div/ul/li[3]/a");
+        public By btnCancelar = By.XPath("//*[@id='tr-negociacao-138669']/td[2]/div/div[1]/div/ul/li[5]/a");
+        public By btnComandos = By.XPath("//*[@id='div-btn-comandos']/button[2]");
+        public By btnGravarSimulacao = By.XPath("//*[@id='btn-gravar-simulacao']");
+        public By mensagem = By.XPath("//*[@id='p-mensagem']");
+        public By abrirOpcoes = By.XPath("//*[@id='tr-negociacao-dados-138675']/td[1]/a");
+        public By btnOpcoesGravados = By.XPath("//*[@id='tr-negociacao-138675']/td[2]/div/div[1]/div/button");
+        public By btnEmitirBoleto = By.XPath("//*[@id='tr-negociacao-138675']/td[2]/div/div[1]/div/ul/li[3]/a");
+        public By btnAbrir = By.XPath("//*[@id='tr-negociacao-138675']/td[2]/div/div[1]/div/ul/li[1]/a");
+        public By btnCancelarNegociacao = By.XPath("//*[@id='tr-negociacao-138675']/td[2]/div/div[1]/div/ul/li[4]/a");
+        public By btnGravarPagamento = By.CssSelector("#btn-gravar-pagamento");
+        public By btnConfirmarPagamento = By.CssSelector("#btn-confirmar-pagamento");
+
+
 
         public VcomCalcPage()
         {
@@ -25,11 +54,27 @@ namespace Vcom.Pages
             Assert.AreEqual(Versao, GetText(By.XPath("//div/div[@class='col-md-4 col-md-offset-4 text-center']/h5/span[text()='" + Versao + "']")));
         }
 
-        public void Negociacao(string data)
+        public void NegociacaoAVista(string data)
         {
             Thread.Sleep(3000);
             BarradeRolagem();
-            Thread.Sleep(4000);
+            Thread.Sleep(5000);
+            WaitElementVisible(inpDataPagamento);
+            ToClick(inpDataPagamento);
+            ToWrite(inpDataPagamento, data);
+            Thread.Sleep(3000);
+            BarradeRolagem();
+            ToClick(btnAtualizar);
+            Thread.Sleep(2000);
+            WaitElementVisible(btnFinalizarNegociacao);
+            ToClick(btnFinalizarNegociacao);
+        }
+
+        public void NegociacaoAParcelado(string data)
+        {
+            Thread.Sleep(3000);
+            BarradeRolagem();
+            Thread.Sleep(5000);
             WaitElementVisible(inpDataPagamento);
             ToClick(inpDataPagamento);
             ToWrite(inpDataPagamento, data);
@@ -47,21 +92,160 @@ namespace Vcom.Pages
             ToClick(btnVisualizarBoleto);
             Thread.Sleep(5000);
         }
+        public void VisualizarEmail()
+        {
+            Thread.Sleep(5000);
+            WaitElementVisible(btnBoletoEmail);
+            ToClick(btnBoletoEmail);
+            WaitElementVisible(selectEmail);
+            ToClick(selectEmail);
+            WaitElementVisible(opcaoEmail);
+            ToClick(opcaoEmail);
+            WaitElementVisible(opcaoEmail);
+            ToClick(btnEnviarEmail);
+            Thread.Sleep(5000);
+            Assert.AreEqual("O boleto foi enviado com sucesso para: ", GetText(By.XPath("//*[@id='div - envio - resultado']/div/h3[text()='O boleto foi enviado com sucesso para: ']")));
+        }
 
+        public void EnviarPorSms()
+        {
+            Thread.Sleep(5000);
+            WaitElementVisible(btnEnviarSMS);
+            ToClick(btnEnviarSMS);
+            BarradeRolagem();
+            WaitElementVisible(selectNumero);
+            ToClick(selectNumero);
+            WaitElementVisible(selectOpcaoNumero);
+            ToClick(selectOpcaoNumero);
+            WaitElementVisible(selectEnvio);
+            ToClick(selectEnvio);
+            WaitElementVisible(selectOpcaoEnvio);
+            ToClick(selectOpcaoEnvio);
+            WaitElementVisible(selectSMS);
+            ToClick(selectSMS);
+            WaitElementVisible(selectOpcaoSMS);
+            ToClick(selectOpcaoSMS);
+            WaitElementVisible(btnEnviarSMSAgora);
+            ToClick(btnEnviarSMSAgora);
+            Thread.Sleep(5000);
+        }
 
-        //public void ValidarPDF()
-        //{
-        //    Thread.Sleep(2000);
-        //    var BtnPDF = Element.Css("a[ng-click='BaixarPDF()']");
-        //    MouseActions.ClickATM(Browser, BtnPDF);
+        public void UltimasAtualizações()
+        {
+            BarradeRolagem();
+            BarradeRolagem();
+            Thread.Sleep(3000);
+            WaitElementVisible(btnNegociacoes);
+            ToClick(btnNegociacoes);
+        }
 
-        //    Thread.Sleep(60000);
+        public void VisualizarUltimasAtualizações()
+        {
+            Thread.Sleep(3000);
+            BarradeRolagem();
+            Thread.Sleep(3000);
+            Assert.AreEqual("Últimas negociações", GetText(By.XPath("//*[@id='div-ultimas-negociacoes']/div/span[text()='Últimas negociações']")));
+        }
 
-        //    AutoItHelper.CancelarImpressao();
+        public void SelecionarReimpressao()
+        {
+            Thread.Sleep(3000);
+            WaitElementVisible(btnMostrarOpcoes);
+            ToClick(btnMostrarOpcoes);
+            WaitElementVisible(btnOpcoes);
+            ToClick(btnOpcoes);
+            Thread.Sleep(3000);
+            WaitElementVisible(btnReimprimir);
+            ToClick(btnReimprimir);
+        }
 
-        //    Browser.SwitchToLastWindow();
-        //    CamposPDF();
+        public void VisualizarReimpressao()
+        {
+            Thread.Sleep(5000);
+        }
 
-        //}
+        public void CancelarBoleto()
+        {
+            Thread.Sleep(3000);
+            WaitElementVisible(btnMostrarOpcoes);
+            ToClick(btnMostrarOpcoes);
+            WaitElementVisible(btnOpcoes);
+            ToClick(btnOpcoes);
+            Thread.Sleep(3000);
+            WaitElementVisible(btnCancelar);
+            ToClick(btnCancelar);
+        }
+
+        public void SalvarNegociacao()
+        {
+            Thread.Sleep(3000);
+            BarradeRolagem();
+            Thread.Sleep(5000);
+            WaitElementVisible(inpDataPagamento);
+            ToClick(inpDataPagamento);
+            ToWrite(inpDataPagamento, "07/12/2020");
+            Thread.Sleep(3000);
+            BarradeRolagem();
+            WaitElementVisible(btnComandos);
+            ToClick(btnComandos);
+            Thread.Sleep(3000);
+            WaitElementVisible(btnGravarSimulacao);
+            ToClick(btnGravarSimulacao);
+        }
+
+        public void EmitirBoleto()
+        {
+            Thread.Sleep(3000);
+            WaitElementVisible(abrirOpcoes);
+            ToClick(abrirOpcoes);
+            WaitElementVisible(btnOpcoesGravados);
+            ToClick(btnOpcoesGravados);
+            Thread.Sleep(3000);
+            WaitElementVisible(btnEmitirBoleto);
+            ToClick(btnCancelar);
+        }
+
+        public void Abrir()
+        {
+            Thread.Sleep(3000);
+            WaitElementVisible(abrirOpcoes);
+            ToClick(abrirOpcoes);
+            WaitElementVisible(btnOpcoesGravados);
+            ToClick(btnOpcoesGravados);
+            Thread.Sleep(3000);
+            WaitElementVisible(btnAbrir);
+            ToClick(btnAbrir);
+        }
+
+        public void Cancelar()
+        {
+            Thread.Sleep(3000);
+            WaitElementVisible(abrirOpcoes);
+            ToClick(abrirOpcoes);
+            WaitElementVisible(btnOpcoesGravados);
+            ToClick(btnOpcoesGravados);
+            Thread.Sleep(3000);
+            WaitElementVisible(btnCancelarNegociacao);
+            ToClick(btnCancelarNegociacao);
+        }
+
+        public void RecebimentoNegociacao()
+        {
+            Thread.Sleep(3000);
+            BarradeRolagem();
+            Thread.Sleep(5000);
+            WaitElementVisible(inpDataPagamento);
+            ToClick(inpDataPagamento);
+            ToWrite(inpDataPagamento, "07/12/2020");
+            Thread.Sleep(3000);
+            BarradeRolagem();
+            WaitElementVisible(btnComandos);
+            ToClick(btnComandos);
+            Thread.Sleep(2000);
+            WaitElementVisible(btnGravarPagamento);
+            ToClick(btnGravarPagamento);
+            Thread.Sleep(5000);
+            WaitElementVisible(btnConfirmarPagamento);
+        }
     }
 }
